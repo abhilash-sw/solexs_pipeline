7e3122cd1ea8868142e661a23c518555f191f55d
Abhilash Sarwade <sarwade@ursc.gov.in>
2022-09-14 10:31:40 +0530

    changed to 0.0.53

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     tests_require=test_requirements,
     #url='https://github.com/leme-cosmo/solexs_pipeline',
     url = 'http://cdeg.isac.dos.gov.in:8081/gitlab/sarwade/solexs_pipeline',
-    version='0.0.52',
+    version='0.0.53',
     zip_safe=False,
 )
