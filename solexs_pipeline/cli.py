2c5a5d60b0ef3601c882e662b977da4f4cf7b3cd
Abhilash Sarwade <as.sarwade@gmail.com>
2023-08-02 08:26:02 +0530

    added pipeline flow

--- solexs_pipeline/cli.py
+++ solexs_pipeline/cli.py
@@ -1,7 +1,8 @@
 """Console script for solexs_pipeline."""
 import sys
 import click
-
+from .L0_interm import intermediate_directory
+from .interm_L1 import L1_directory
 
 @click.command()
 def main(args=None):
@@ -9,6 +10,22 @@ def main(args=None):
     click.echo("Replace this message by putting your code into "
                "solexs_pipeline.cli.main")
     click.echo("See click documentation at https://click.palletsprojects.com/")
+
+    filename = click.argument('filename',type=click.Path(exists=True))
+
+    # Intermediate Directory
+    interm_dir = intermediate_directory(filename)
+    interm_dir.make_interm_dir(filename)
+    interm_dir.write_interm_files(SDD_number=1)
+    interm_dir.write_interm_files(SDD_number=2)
+
+    # L1 Directory
+    interm_dir_path = interm_dir.output_dir
+    l1_dir = L1_directory(interm_dir_path)
+    l1_dir.make_l1_dir()
+    l1_dir.write_l1_files(SDD_number=1)
+    l1_dir.write_l1_files(SDD_number=2)
+
     return 0
 
 
