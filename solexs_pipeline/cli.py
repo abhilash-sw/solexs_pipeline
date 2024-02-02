8c81ad5c9315ce80cc356e110947e16b5eb990f5
Abhilash Sarwade <as.sarwade@gmail.com>
2024-02-02 07:15:11 +0530

    click options: case insensitive variable name

--- solexs_pipeline/cli.py
+++ solexs_pipeline/cli.py
@@ -12,8 +12,8 @@ log = setup_logger('solexs_pipeline')
 @click.option('-i', '--input_file', help='Input SoLEXS Instrument Data File', multiple=True)
 @click.option('-o', '--output_dir', default=None, show_default=True, help='Output Directory')
 @click.option('-dt', '--data_type', default='L0', show_default=True, help='Raw/SP/L0')
-@click.option('-s', '--SDD', default='12', show_default=True, help='1/2/12')
-def main(input_file,output_dir,data_type,SDD,args=None):
+@click.option('-sdd', '--SDD', default='12', show_default=True, help='1/2/12')
+def main(input_file,output_dir,data_type,sdd,args=None):
     """Console script for solexs_pipeline."""
     click.echo("SoLEXS pipeline command line interface "
                "solexs_pipeline.cli.main")
@@ -36,12 +36,12 @@ def main(input_file,output_dir,data_type,SDD,args=None):
         interm_date = interm_dir.solexs_bd.pld_header_SDD1.pld_utc_datetime[0].strftime('%Y%m%d')
         interm_output_dir = os.path.join(output_dir,'AL1_SOLEXS_'+ interm_date,'intermediate')
         interm_dir.make_interm_dir(output_dir=interm_output_dir)
-        if SDD=='12':
+        if sdd=='12':
             interm_dir.write_interm_files(SDD_number=1)
             interm_dir.write_interm_files(SDD_number=2)
-        elif SDD=='1':
+        elif sdd=='1':
             interm_dir.write_interm_files(SDD_number=1)
-        elif SDD=='2':
+        elif sdd=='2':
             interm_dir.write_interm_files(SDD_number=2)
 
         interm_dir_paths.append(interm_dir.output_dir)
@@ -50,7 +50,7 @@ def main(input_file,output_dir,data_type,SDD,args=None):
     log.info('Initiating intermediate to L1 data pipeline.')
     # interm_dir_path = interm_dir.output_dir
     l1_dir = L1_directory(interm_dir_paths)
-    if SDD=='12':
+    if sdd=='12':
         l1_pi_file_sdd1, l1_lc_file_sdd1 = l1_dir.create_l1_files(SDD_number=1)
         l1_dir.make_l1_dir(output_dir)
         l1_dir.write_l1_files(1, l1_pi_file_sdd1, l1_lc_file_sdd1)
@@ -58,12 +58,12 @@ def main(input_file,output_dir,data_type,SDD,args=None):
         l1_pi_file_sdd2, l1_lc_file_sdd2 = l1_dir.create_l1_files(SDD_number=2)
         l1_dir.write_l1_files(2, l1_pi_file_sdd2, l1_lc_file_sdd2)
     
-    if SDD=='1':
+    if sdd=='1':
         l1_pi_file_sdd1, l1_lc_file_sdd1 = l1_dir.create_l1_files(SDD_number=1)
         l1_dir.make_l1_dir(output_dir)
         l1_dir.write_l1_files(1, l1_pi_file_sdd1, l1_lc_file_sdd1)
 
-    if SDD=='2':
+    if sdd=='2':
         l1_pi_file_sdd2, l1_lc_file_sdd2 = l1_dir.create_l1_files(SDD_number=2)
         l1_dir.make_l1_dir(output_dir)
         l1_dir.write_l1_files(2, l1_pi_file_sdd2, l1_lc_file_sdd2)
